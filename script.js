49aa19491ef653ed412855a9dc4723f9f3b2cc14
Ramsha <fnuramsha@gmail.com>
2024-04-25 12:42:44 +0200

    implemented part 1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const whereAmi = function (lat, lng) {
+  return fetch(
+    `https://geocode.xyz/${lat},${lng}?geoit=json&auth=280979820488412507527x50367`
+  )
+    .then((response) => response.json())
+    .then((res) => console.log(`You're in ${res.city}, ${res.country}`));
+};
+
+whereAmi(52.508, 13.381);
+whereAmi(19.037, 72.873);
+whereAmi(-33.933, 18.474);
